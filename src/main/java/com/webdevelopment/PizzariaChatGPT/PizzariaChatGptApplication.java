e84bbdb1161aab25a163955943bbf95f5887255c
Guilherme Zampronio <guizampronio@gmail.com>
2023-03-20 23:19:27 -0300

    First commit

---
+++ src/main/java/com/webdevelopment/PizzariaChatGPT/PizzariaChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.webdevelopment.PizzariaChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaChatGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdevelopment/PizzariaChatGPT/PizzariaChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdevelopment.PizzariaChatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
